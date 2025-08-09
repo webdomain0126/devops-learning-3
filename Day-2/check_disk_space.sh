32970306bcb8d580586ff1c29077ae8ca2fd93d5
Tisa Akhter <100247981+webdomain0126@users.noreply.github.com>
2025-08-09 22:40:19 +0600

    Update check_disk_space.sh

--- Day-2/check_disk_space.sh
+++ Day-2/check_disk_space.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# This script checks disk usage and shows any partitions
+# where usage is over 80%
+
+# 'df -h' shows disk usage in human-readable format (like GB, MB)
+# 'awk' filters lines where the 5th column (Use%) is greater than 80%
+df -h | awk '$5+0 > 80 {print}'
 
