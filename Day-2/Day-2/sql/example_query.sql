d6c03ab58c557e9cc0839b73746aa93fb81fe2ab
Tisa Akhter <100247981+webdomain0126@users.noreply.github.com>
2025-08-10 12:49:11 +0600

    Delete Day-2 directory

--- Day-2/Day-2/sql/example_query.sql
+++
@@ -1,7 +0,0 @@
--- This SQL query selects all users from Bangladesh
--- SELECT * means select all columns
--- FROM users means from the table named 'users'
--- WHERE country = 'Bangladesh' filters rows with country = Bangladesh
-
-SELECT * FROM users WHERE country = 'Bangladesh';
-
