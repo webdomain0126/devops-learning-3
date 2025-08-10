ee1126a35652a7e5fb8b93d763b3e0f0b186ea83
Tisa Akhter <100247981+webdomain0126@users.noreply.github.com>
2025-08-10 12:39:26 +0600

    Update example_query.sql

--- Day-2/Day-2/sql/example_query.sql
+++ Day-2/Day-2/sql/example_query.sql
@@ -1,1 +1,7 @@
+-- This SQL query selects all users from Bangladesh
+-- SELECT * means select all columns
+-- FROM users means from the table named 'users'
+-- WHERE country = 'Bangladesh' filters rows with country = Bangladesh
+
+SELECT * FROM users WHERE country = 'Bangladesh';
 
